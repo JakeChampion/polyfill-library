858b94a90272aac7bdbb7a5ab07d41edd0977fdf
Jake Champion <jakechampion.jake2@googlemail.com>
2019-06-21 16:03:51 +0100

    Only run test in browsers which support getPrototypeOf

--- polyfills/Reflect/tests.js
+++ polyfills/Reflect/tests.js
@@ -9,9 +9,11 @@ it('is not enumerable', function () {
     proclaim.isNotEnumerable(window, 'Reflect');
 });
 
-it('has a prototype of Object.prototype', function () {
-    proclaim.deepStrictEqual(Object.getPrototypeOf(Reflect), Object.prototype);
-});
+if ('getPrototypeOf' in Object) {
+    it('has a prototype of Object.prototype', function () {
+        proclaim.deepStrictEqual(Object.getPrototypeOf(Reflect), Object.prototype);
+    });
+}
 
 it('cannot be called or constructed', function () {
     proclaim.throws(function () {
