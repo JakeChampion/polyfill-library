2a4d2cf092c8010f6a6f36b80a6683269f0386e5
Jake Champion <jakechampion.jake2@googlemail.com>
2019-06-23 23:52:27 +0100

    only run test in browsers which have __proto__ or Object.setPrototypeOf

--- polyfills/Reflect/setPrototypeOf/tests.js
+++ polyfills/Reflect/setPrototypeOf/tests.js
@@ -41,23 +41,24 @@ it('returns false if target and proto are the same, without setting a new protot
     proclaim.equal(Object.getPrototypeOf(o), Object.prototype);
 });
 
-it('returns true if the new prototype is set', function () {
-    var o1 = {};
-    proclaim.isTrue(Reflect.setPrototypeOf(o1, null));
-    proclaim.equal(Object.getPrototypeOf(o1), null);
+if (Object.setPrototypeOf) {
+    it('returns true if the new prototype is set', function () {
+        var o1 = {};
+        proclaim.isTrue(Reflect.setPrototypeOf(o1, null));
+        proclaim.equal(Object.getPrototypeOf(o1), null);
 
-    if ('create' in Object) {
-        var o2 = Object.create(null);
-        proclaim.isTrue(Reflect.setPrototypeOf(o2, Object.prototype));
-        proclaim.equal(Object.getPrototypeOf(o2), Object.prototype);
-    }
+        if ('create' in Object) {
+            var o2 = Object.create(null);
+            proclaim.isTrue(Reflect.setPrototypeOf(o2, Object.prototype));
+            proclaim.equal(Object.getPrototypeOf(o2), Object.prototype);
+        }
 
-    var o3 = {};
-    var proto = {};
-    proclaim.isTrue(Reflect.setPrototypeOf(o3, proto));
-    proclaim.equal(Object.getPrototypeOf(o3), proto);
-});
-
+        var o3 = {};
+        var proto = {};
+        proclaim.isTrue(Reflect.setPrototypeOf(o3, proto));
+        proclaim.equal(Object.getPrototypeOf(o3), proto);
+    });
+}
 it('throws a TypeError if target is not an Object', function () {
     proclaim.throws(function () {
         Reflect.setPrototypeOf(1, {});
