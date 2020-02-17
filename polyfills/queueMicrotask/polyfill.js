037ba8da386a8ac2f4f2b648c8c3f8225a941639
Jake Champion <jakechampion.jake2@googlemail.com>
2020-02-17 15:34:08 +0000

    Add error messages

--- polyfills/queueMicrotask/polyfill.js
+++ polyfills/queueMicrotask/polyfill.js
@@ -2,11 +2,11 @@
 	var promise = global.Promise.resolve();
 	global.queueMicrotask = function queueMicrotask(microtask) {
 		if (arguments.length > 1) {
-			throw new TypeError();
+			throw new TypeError("Window.queueMicrotask requires at least 1 argument, but only 0 were passed");
 		}
 
 		if (typeof(microtask) != 'function'){
-			throw new TypeError();
+			throw new TypeError('Argument 1 of Window.queueMicrotask is not callable.');
 		}
 
 		promise = promise.then(function(){
