290c63fd537d67abd668a2fe608f37fd081edc91
Jake Champion <jakechampion.jake2@googlemail.com>
2020-02-17 16:54:40 +0000

    Update polyfill.js

--- polyfills/queueMicrotask/polyfill.js
+++ polyfills/queueMicrotask/polyfill.js
@@ -1,14 +1,11 @@
-(function (global) {
-	var promise = global.Promise.resolve();
-	global.queueMicrotask = function queueMicrotask(microtask) {
-		if (arguments.length < 1) {
-			throw new TypeError("Window.queueMicrotask requires at least 1 argument, but only 0 were passed");
-		}
+self.queueMicrotask = function queueMicrotask(microtask) {
+	if (arguments.length < 1) {
+		throw new TypeError("Window.queueMicrotask requires at least 1 argument, but only 0 were passed");
+	}
 
-		if (typeof(microtask) != 'function'){
-			throw new TypeError('Argument 1 of Window.queueMicrotask is not callable.');
-		}
+	if (typeof(microtask) != 'function'){
+		throw new TypeError('Argument 1 of Window.queueMicrotask is not callable.');
+	}
 
-		promise = promise.then(microtask);
-	};
-}(this))
+	Promise.resolve().then(microtask);
+};
