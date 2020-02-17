3ff5801745cbc7cc85923ae35f93fa1b6c763645
Jake Champion <jakechampion.jake2@googlemail.com>
2020-02-17 17:07:34 +0000

    Remove `Window.` as queueMicrotask exists in Worker environments as well

--- polyfills/queueMicrotask/polyfill.js
+++ polyfills/queueMicrotask/polyfill.js
@@ -1,11 +1,11 @@
 /* global Promise */
 self.queueMicrotask = function queueMicrotask(microtask) {
 	if (arguments.length < 1) {
-		throw new TypeError("Window.queueMicrotask requires at least 1 argument, but only 0 were passed");
+		throw new TypeError("queueMicrotask requires at least 1 argument, but only 0 were passed");
 	}
 
 	if (typeof(microtask) != 'function'){
-		throw new TypeError('Argument 1 of Window.queueMicrotask is not callable.');
+		throw new TypeError('Argument 1 of queueMicrotask is not callable.');
 	}
 
 	Promise.resolve().then(microtask);
