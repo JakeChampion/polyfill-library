671b72663f99dfc98859801d604ef85636b0fc08
Jake Champion <me@jakechampion.name>
2020-02-20 13:42:39 +0000

    dispatch an error event if the microtask callback throws, do not fire an unhandled promise rejection event by attaching a catch handler for the promise

--- polyfills/queueMicrotask/polyfill.js
+++ polyfills/queueMicrotask/polyfill.js
@@ -1,12 +1,34 @@
 /* global Promise */
 self.queueMicrotask = function queueMicrotask(microtask) {
-	if (arguments.length < 1) {
-		throw new TypeError("queueMicrotask requires at least 1 argument, but only 0 were passed");
-	}
+  if (arguments.length < 1) {
+    throw new TypeError(
+      "queueMicrotask requires at least 1 argument, but only 0 were passed"
+    );
+  }
 
-	if (typeof(microtask) != 'function'){
-		throw new TypeError('Argument 1 of queueMicrotask is not callable.');
-	}
+  if (typeof microtask != "function") {
+    throw new TypeError("Argument 1 of queueMicrotask is not callable.");
+  }
 
-	Promise.resolve().then(microtask);
+  Promise.resolve()
+    .then(function() {
+      try {
+        microtask();
+      } catch (e) {
+        self.dispatchEvent(
+          new ErrorEvent("error", {
+            message: e.message,
+            filename: e.filename,
+            lineno: e.lineno,
+            colno: e.colno,
+            error: e,
+            bubbles: true,
+            cancelable: true,
+            composed: false
+          })
+        );
+        throw e;
+      }
+    })
+    .catch(function() {});
 };
