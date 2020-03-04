6c333f3430f132c1e116e6b70306cc47196f3c23
Jake Champion <me@jakechampion.name>
2020-03-04 16:16:47 +0000

    reformat file

--- test/polyfills/update-polyfill-targeting.js
+++ test/polyfills/update-polyfill-targeting.js
@@ -7,74 +7,121 @@ const polyfillLibrary = require("../../lib/index");
 const TOML = require("@iarna/toml");
 
 async function main() {
-	const file = path.join(__dirname, "./compat.json");
-	// Ensure file exists before proceeding.
-	if (!fs.existsSync(file)) {
-		throw new Error("Compat results file does not exists, to create the file you need to run the command: `node ./test/polyfills/compat.js`.");
-	}
-	const compat = await fs.readJSON(file);
-	const changes = [];
-	for (let feature of Object.keys(compat)) {
-		feature = feature.split(" ")[0];
-		const featureMetadata = await polyfillLibrary.describePolyfill(feature);
-		if (!featureMetadata) {
-			throw new Error(`${feature} does not exists within the polyfill-library.`);
-		}
+  const file = path.join(__dirname, "./compat.json");
+  // Ensure file exists before proceeding.
+  if (!fs.existsSync(file)) {
+    throw new Error(
+      "Compat results file does not exists, to create the file you need to run the command: `node ./test/polyfills/compat.js`."
+    );
+  }
+  const compat = await fs.readJSON(file);
+  const changes = [];
+  for (let feature of Object.keys(compat)) {
+    feature = feature.split(" ")[0];
+    const featureMetadata = await polyfillLibrary.describePolyfill(feature);
+    if (!featureMetadata) {
+      throw new Error(
+        `${feature} does not exists within the polyfill-library.`
+      );
+    }
 
-		for (const browser of Object.keys(compat[feature])) {
-			for (const [version, support] of Object.entries(compat[feature][browser])) {
-				if (support === "native") {
-					if (featureMetadata.browsers && featureMetadata.browsers[browser] && semver.satisfies(semver.coerce(version), featureMetadata.browsers[browser])) {
-						changes.push([feature + "|" + browser, JSON.stringify({ [browser]: `<${version}` })]);
-					}
-				}
-				if (support === "polyfilled") {
-					if (!featureMetadata.browsers || !featureMetadata.browsers[browser] || !semver.satisfies(semver.coerce(version), featureMetadata.browsers[browser])) {
-						changes.push([feature + "|" + browser, JSON.stringify({ [browser]: `<${Number.parseFloat(version) + 1}` })]);
-					}
-				}
+    for (const browser of Object.keys(compat[feature])) {
+      for (const [version, support] of Object.entries(
+        compat[feature][browser]
+      )) {
+        if (support === "native") {
+          if (
+            featureMetadata.browsers &&
+            featureMetadata.browsers[browser] &&
+            semver.satisfies(
+              semver.coerce(version),
+              featureMetadata.browsers[browser]
+            )
+          ) {
+            changes.push([
+              feature + "|" + browser,
+              JSON.stringify({ [browser]: `<${version}` })
+            ]);
+          }
+        }
+        if (support === "polyfilled") {
+          if (
+            !featureMetadata.browsers ||
+            !featureMetadata.browsers[browser] ||
+            !semver.satisfies(
+              semver.coerce(version),
+              featureMetadata.browsers[browser]
+            )
+          ) {
+            changes.push([
+              feature + "|" + browser,
+              JSON.stringify({
+                [browser]: `<${Number.parseFloat(version) + 1}`
+              })
+            ]);
+          }
+        }
 
-				if (support === "missing") {
-					if (!featureMetadata.browsers || !featureMetadata.browsers[browser] || !semver.satisfies(semver.coerce(version), featureMetadata.browsers[browser])) {
-						changes.push([feature + "|" + browser,JSON.stringify({ [browser]: `<${Number.parseFloat(version) + 1}` })]);
-					}
-				}
-			}
-		}
-	}
+        if (support === "missing") {
+          if (
+            !featureMetadata.browsers ||
+            !featureMetadata.browsers[browser] ||
+            !semver.satisfies(
+              semver.coerce(version),
+              featureMetadata.browsers[browser]
+            )
+          ) {
+            changes.push([
+              feature + "|" + browser,
+              JSON.stringify({
+                [browser]: `<${Number.parseFloat(version) + 1}`
+              })
+            ]);
+          }
+        }
+      }
+    }
+  }
 
-	async function updateFeature(feature, update) {
-		const configPath = path.join(__dirname, "../../polyfills", feature.join("/").replace('.', '/'), "config.toml");
-		const config = TOML.parse(await fs.readFile(configPath, 'utf-8'));
-		config.browsers = config.browsers || {};
-		config.browsers = Object.assign(config.browsers, JSON.parse(update));
-        await fs.writeFile(configPath, TOML.stringify(config), 'utf-8');
-	}
-	if (changes.length > 0) {
-		for (const [featureWithBrowser, value] of changes) {
-			const [feature] = featureWithBrowser.split("|");
-			if (typeof value === "object") {
-				for (const [featureWithBrowser2, value2] of Object.entries(value)) {
-					const [feature2] = featureWithBrowser2.split("|");
-					if (typeof value2 === "object") {
-						for (const [featureWithBrowser3, value3] of Object.entries(value2)) {
-							const [feature3] = featureWithBrowser3.split("|");
-							await updateFeature([feature, feature2, feature3], value3);
-						}
-					} else {
-						await updateFeature([feature, feature2], value2);
-					}
-				}
-			} else {
-				await updateFeature([feature], value);
-			}
-		}
-	} else {
-		console.log("All browsers have correctly configured polyfills. Nice!");
-	}
+  async function updateFeature(feature, update) {
+    const configPath = path.join(
+      __dirname,
+      "../../polyfills",
+      feature.join("/").replace(".", "/"),
+      "config.toml"
+    );
+    const config = TOML.parse(await fs.readFile(configPath, "utf-8"));
+    config.browsers = config.browsers || {};
+    config.browsers = Object.assign(config.browsers, JSON.parse(update));
+    await fs.writeFile(configPath, TOML.stringify(config), "utf-8");
+  }
+  if (changes.length > 0) {
+    for (const [featureWithBrowser, value] of changes) {
+      const [feature] = featureWithBrowser.split("|");
+      if (typeof value === "object") {
+        for (const [featureWithBrowser2, value2] of Object.entries(value)) {
+          const [feature2] = featureWithBrowser2.split("|");
+          if (typeof value2 === "object") {
+            for (const [featureWithBrowser3, value3] of Object.entries(
+              value2
+            )) {
+              const [feature3] = featureWithBrowser3.split("|");
+              await updateFeature([feature, feature2, feature3], value3);
+            }
+          } else {
+            await updateFeature([feature, feature2], value2);
+          }
+        }
+      } else {
+        await updateFeature([feature], value);
+      }
+    }
+  } else {
+    console.log("All browsers have correctly configured polyfills. Nice!");
+  }
 }
 
 main().catch(e => {
-	console.error(e);
-	process.exitCode = 1;
+  console.error(e);
+  process.exitCode = 1;
 });
