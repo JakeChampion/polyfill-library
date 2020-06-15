6dd542dd9d325b089f961007a0c066ce02bf3a0a
romainmenke <romainmenke@gmail.com>
2020-06-15 21:07:39 +0200

    FormData : use the same detect logic as the actual polyfill

--- polyfills/FormData/detect.js
+++ polyfills/FormData/detect.js
@@ -1,13 +1,1 @@
-'FormData' in self && (function(self) {
-	try {
-		var formData = new self.FormData();
-		// FormData as iterator is the newest feature.
-		if ('Symbol' in self && 'iterator' in self.Symbol && typeof formData[self.Symbol.iterator] === 'function') {
-			return true;
-		}
-
-		return false;
-	} catch (e) {
-		return false;
-	}
-}(self))
+'FormData' in self && self.FormData.prototype.keys
