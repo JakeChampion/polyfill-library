f88b80126593b649c029c61a4d5c13e981f3df44
Long Ho <holevietlong@gmail.com>
2023-11-14 00:25:34 -0500

    upgrade formatjs polyfills
    This includes NumberFormat v3 as well

--- polyfills/Intl/NumberFormat/detect.js
+++ polyfills/Intl/NumberFormat/detect.js
@@ -1,11 +1,21 @@
-'Intl' in self && 'NumberFormat' in self.Intl && (function () {
-	try {
-		new Intl.NumberFormat(undefined, {
-			style: 'unit',
-			unit: 'byte'
-		});
-	} catch (e) {
-		return false;
-	}
-	return true;
-})()
+"Intl" in self &&
+	"NumberFormat" in self.Intl &&
+	(function supportsES2020() {
+		try {
+			const s = new self.Intl.NumberFormat("en", {
+				style: "unit",
+				unit: "bit",
+				unitDisplay: "long",
+				notation: "scientific"
+			}).format(10000);
+
+			// Check for a plurality bug in environment that uses the older versions of ICU:
+			// https://unicode-org.atlassian.net/browse/ICU-13836
+			if (s !== "1E4 bits") {
+				return false;
+			}
+		} catch (e) {
+			return false;
+		}
+		return true;
+	})();
--- polyfills/Intl/getCanonicalLocales/detect.js
+++ polyfills/Intl/getCanonicalLocales/detect.js
@@ -1,1 +1,5 @@
-'Intl' in self && 'getCanonicalLocales' in self.Intl
+"Intl" in self &&
+	"getCanonicalLocales" in self.Intl &&
+	// Native Intl.getCanonicalLocales is just buggy
+	// https://bugs.chromium.org/p/v8/issues/detail?id=10682
+	self.Intl.getCanonicalLocales("und-x-private")[0] !== "x-private";
